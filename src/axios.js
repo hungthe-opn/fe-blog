7ec8d4a6999fced33931fee1aa96cc1dd0698365
hungnt <hung.nguyen.the@ofatech.jsc.com>
2022-12-12 18:01:51 +0700

    Base-BE-Auth-Google

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://127.0.0.1:8001/api/'
+const baseURL = 'http://127.0.0.1:8000/api/'
 
 // axios custom JWT
 export const axiosInstance = axios.create({
